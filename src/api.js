7f1c6c0cb741da5e27ba0b1cdb5d70716b276344
SAIKIRANghb <saikiransuguru@gmail.com>
2025-05-05 15:10:50 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
 
 // export const API_URL = 'http://192.168.137.1:5001';
-export const API_URL = 'http://localhost:5001';
+// export const API_URL = 'http://localhost:5001';
 
-// export const API_URL = 'https://moviebookingbackend-fu14.onrender.com';
+export const API_URL = 'https://moviebookingbackend-fu14.onrender.com';
